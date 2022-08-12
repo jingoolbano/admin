9edf6f1f3279eadbe5ed792b7da2d6cbe07e62f5
Mina <minaaaaskary@gmail.com>
2022-08-12 13:03:54 +0430

    side bar end

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,7 +1,13 @@
 import "./home.scss";
+import Sidebar from "../../components/sidebar/Sidebar";
 
 const Home = () => {
-  return <></>;
+  return (
+    <div className="home">
+      <Sidebar />
+      <div className="homeContainer">Container</div>
+    </div>
+  );
 };
 
 export default Home;
