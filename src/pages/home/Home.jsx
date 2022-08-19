33d1909f01ca92b47f463b193b91e27cffa4b467
Mina <minaaaaskary@gmail.com>
2022-08-19 17:33:49 +0430

    end of chart, table, home page

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -4,6 +4,7 @@ import Navbar from "../../components/navbar/Navbar";
 import Widget from "../../components/widgets/Widget";
 import Featured from "../../components/featured/Featured";
 import Chart from "../../components/chart/Chart";
+import List from "../../components/table/Table";
 
 const Home = () => {
   return (
@@ -20,6 +21,10 @@ const Home = () => {
         <div className="charts">
           <Featured />
           <Chart />
+        </div>
+        <div className="listContainer">
+          <div className="title">Latest transaction</div>
+          <List />
         </div>
       </div>
     </div>
